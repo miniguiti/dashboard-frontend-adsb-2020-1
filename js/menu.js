5ef4666e245817ada6734a74728f4ea817496cf3
Giuliana Miniguiti França <gminiguiti@outlook.com>
2020-06-02 14:56:59 -0300

    V4 - Formulário de contato e rafatoração

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
